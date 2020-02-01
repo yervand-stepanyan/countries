3b10aa633b9fe12d0ba9ffe3b9733f50dd369784
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-02-01 20:47:34 +0400

    Add Prettier config file

--- src/Components/Countries/Countries.js
+++ src/Components/Countries/Countries.js
@@ -7,7 +7,6 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 
-
 export default class Countries extends React.Component {
   constructor(props) {
     super(props);
@@ -15,85 +14,107 @@ export default class Countries extends React.Component {
     this.state = {
       isEmpty: false,
       isLoading: false,
-      errorText: "",
-      countries: [],
-    }
+      errorText: '',
+      countries: []
+    };
   }
 
   componentDidMount() {
-    this.setState({isLoading: true});
+    this.setState({ isLoading: true });
 
     this.fetchCountries();
   }
 
-  fetchCountries = (countryName = "") => {
+  fetchCountries = (countryName = '') => {
     if (countryName) {
       fetch(`https://restcountries.eu/rest/v2/name/${countryName}`)
         .then(res => res.json())
-        .then((res) => {
+        .then(res => {
           if (res.status === 404) {
-            throw new Error('No Data')
+            throw new Error('No Data');
           }
           return res;
         })
-        .then(res => this.setState({
-          countries: res, isEmpty: res.length === 0,
-          isLoading: false, errorText: ""
-        }))
-        .catch(err => this.setState({isLoading: false, errorText: err.message, countries: []}));
+        .then(res =>
+          this.setState({
+            countries: res,
+            isEmpty: res.length === 0,
+            isLoading: false,
+            errorText: ''
+          })
+        )
+        .catch(err =>
+          this.setState({
+            isLoading: false,
+            errorText: err.message,
+            countries: []
+          })
+        );
     } else {
       fetch('https://restcountries.eu/rest/v2/all')
         .then(res => res.json())
-        .then(res => this.setState({countries: res, isEmpty: res.length === 0, isLoading: false}))
-        .catch(err => this.setState({isLoading: false, errorText: err.message}));
+        .then(res =>
+          this.setState({
+            countries: res,
+            isEmpty: res.length === 0,
+            isLoading: false
+          })
+        )
+        .catch(err =>
+          this.setState({ isLoading: false, errorText: err.message })
+        );
     }
   };
 
-  searchCountry = (countryName) => {
-    this.setState({isLoading: true});
+  searchCountry = countryName => {
+    this.setState({ isLoading: true });
 
     this.fetchCountries(countryName);
   };
 
   render() {
-    const {isEmpty, isLoading, errorText, countries} = this.state;
+    const { isEmpty, isLoading, errorText, countries } = this.state;
 
     return (
       <div className="main">
-        <Input onInputClick={this.searchCountry}/>
+        <Input onInputClick={this.searchCountry} />
         <div className="countryWrapper">
-          {errorText
-            ? <p>{errorText}</p>
-            : isEmpty
-              ? <p>No Data</p>
-              : isLoading
-                ? <p>Loading ...</p>
-                : countries.map(country =>
-                  <Card key={country.name} className="cardStyle">
-                    <CardContent className="cardContent">
-                      <Typography
-                        className="countryName"
-                        component="h2"
-                        gutterBottom color="primary">
-                        {country.name}
-                      </Typography>
-                      <Typography variant="h6" gutterBottom color="textSecondary">
-                        {country.capital}
-                      </Typography>
-                      <Typography variant="h6" gutterBottom color="textSecondary">
-                        Domain: {country.topLevelDomain}
-                      </Typography>
-                      <Typography variant="body2" component="p">
-                        Population: {country.population}
-                      </Typography>
-                      <CardMedia
-                        className="cardFlag"
-                        image={country.flag}
-                        title="Country flag"
-                      />
-                    </CardContent>
-                  </Card>)
-          }
+          {errorText ? (
+            <p>{errorText}</p>
+          ) : isEmpty ? (
+            <p>No Data</p>
+          ) : isLoading ? (
+            <p>Loading ...</p>
+          ) : (
+            countries.map(country => (
+              <Card key={country.name} className="cardStyle">
+                <CardContent className="cardContent">
+                  <Typography
+                    className="countryName"
+                    component="h2"
+                    gutterBottom
+                    color="primary"
+                  >
+                    {country.name}
+                  </Typography>
+                  <Typography variant="h6" gutterBottom color="textSecondary">
+                    {country.capital}
+                  </Typography>
+                  <Typography variant="h6" gutterBottom color="textSecondary">
+                    Domain: {country.topLevelDomain}
+                  </Typography>
+                  <Typography variant="body2" component="p">
+                    Population: {country.population}
+                  </Typography>
+                  <CardMedia
+                    className="cardFlag"
+                    image={country.flag}
+                    title="Country flag"
+                  />
+                </CardContent>
+              </Card>
+            ))
+          )}
         </div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Countries from "./Components/Countries/Countries";
+import Countries from './Components/Countries/Countries';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Countries />, document.getElementById('root'));
