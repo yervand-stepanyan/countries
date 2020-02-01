48142f5679e094e3d348abc3108cff966f33bfad
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-02-01 21:05:57 +0400

    Change index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Countries from './Components/Countries/Countries';
 import * as serviceWorker from './serviceWorker';
+import Countries from './Components/Countries/Countries';
 
 ReactDOM.render(<Countries />, document.getElementById('root'));
 
