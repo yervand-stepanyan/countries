2a17c43b57fef3561514cfc300f0b8d815000122
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-03-15 12:40:25 +0400

    Add Redirect to fix root url issue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
 
 import Countries from './Components/Countries/Countries';
 import CountryDetails from './Components/CountryDetails/CountryDetails';
@@ -8,6 +13,9 @@ export default function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/countries">
+          <Redirect to={{ pathname: '/' }} />
+        </Route>
         <Route exact path="/" component={Countries} />
         <Route path="/:name" component={CountryDetails} />
       </Switch>
